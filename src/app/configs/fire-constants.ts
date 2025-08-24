e96ed6979e18f47a1d80994aa6ba86c0f1d72ee6
Teja Chittala <chittala.teja6969@gmail.com>
2025-08-24 13:01:39 +0530

    Login Design Changes

---
+++ src/app/configs/fire-constants.ts
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlNmgqXqIc9zgdMa_QpDWeqJkGRKbR48E",
+  authDomain: "society-finance-67af2.firebaseapp.com",
+  databaseURL: "https://society-finance-67af2-default-rtdb.firebaseio.com",
+  projectId: "society-finance-67af2",
+  storageBucket: "society-finance-67af2.firebasestorage.app",
+  messagingSenderId: "68770534547",
+  appId: "1:68770534547:web:10329f3dd79df4d90e6966"
+};
