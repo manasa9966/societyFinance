b12d77cc2f8093f56595680d0f19569a75b12041
Teja Chittala <chittala.teja6969@gmail.com>
2025-08-25 16:53:58 +0530

    family Changes

---
+++ src/app/constants/menu.ts
@@ -0,0 +1,16 @@
+export const adminMenu = [
+  { label: 'Dashboard', icon: 'dashboard', route: 'dashboardPage' },
+  { label: 'Families', icon: 'group', route: 'family' },
+  { label: 'Payments', icon: 'payments', route: 'payments' },
+  { label: 'Outward Requests', icon: 'assignment', route: 'requests' },
+  { label: 'Activities', icon: 'event', route: 'activities' },
+  { label: 'Invoices', icon: 'insert_chart', route: 'reports' },
+];
+
+export const userMenu = [
+  { label: 'Dashboard', icon: 'dashboard', route: 'dashboard' },
+  { label: 'My Payments', icon: 'payment', route: 'payments' },
+  { label: 'Outward Requests', icon: 'assignment', route: 'requests' },
+  { label: 'Complaints', icon: 'chat_bubble_outline', route: 'complaints' },
+  { label: 'Help & Support', icon: 'help_outline', route: 'help' }
+];
